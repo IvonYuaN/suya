e43af1edec366481d71dfb79bceec05ac6abfa13
Apollo&丸子君 <info@eoen.org>
2021-04-11 17:43:40 +0800

    v1.0.5
    更新微信登录接口

--- pages/user/login.vue
+++ pages/user/login.vue
@@ -9,7 +9,8 @@
 	        <image class="logo" :src="shopData.shop_logo" />
 	        <view class="title" :style="'color:'+template.color.color2">{{shopData.shop_name}}</view>
 	        <view class="text" :style="'color:'+template.color.color3">首次进入需要授权才能使用哦</view>
-	        <button class="userlogin" :style="'background-color:'+template.color.color1" open-type="getUserInfo" @getuserinfo="userlogin">授权登录</button>
+	        <!-- <button class="userlogin" :style="'background-color:'+template.color.color1" open-type="getUserInfo" @getuserinfo="userlogin">授权登录</button> -->
+	       <view class="button" :style="'background-color:'+template.color.color1">立即登录<button @click="getUserInfoTap"></button></view>
 	        <button class="userlogin navigateBack" @click="navigateBack()">暂不登陆</button>
 	    </view>
 	</view>
@@ -24,12 +25,69 @@
 			return {
 				template: uni.getStorageSync('__suyaTemplateConfig'),
 				shopData: uni.getStorageSync('__suyaShopInfo'),
+				wxCode: ''
 			}
 		},
 		onLoad() {
-
+			let self = this;
+			self.getWechatLogin();
 		},
 		methods: {
+			getWechatLogin() {
+				let self = this;
+				uni.login({
+					provider: 'weixin',
+					success: function(wxs) {
+						self.wxCode = wxs.code;
+					}
+				})
+			},
+			//新版登录接口
+			getUserInfoTap() {
+				let self = this;
+				uni.showLoading({title: '正在登录中'});
+				uni.getUserProfile({
+					desc: '注册身份信息验证',
+					success(ues) {
+						let user = ues;
+						let postData = {
+							iv: user.iv,
+							code: self.wxCode,
+							raw_data: user.rawData,
+							signature: user.signature,
+							encrypted_data: user.encryptedData
+						}
+						self.Post(self.Url.wechatDetail, postData).then(wes => {
+							if(wes.code === 0){
+								uni.hideLoading();
+								app.globalData.user = true;
+								uni.showToast({title: '授权登陆成功'});
+								uni.setStorage({key: '__suyaUserInfo',data: res.data});
+								setTimeout(function () {
+									uni.navigateBack();
+								}, 500);
+							}else{
+								self.getWechatLogin();
+								uni.hideLoading();
+								uni.showModal({
+								  title: '授权失败',
+								  content: wes.msg,
+								  showCancel: false
+								});
+							}
+						});
+					},
+					fail() {
+						self.getWechatLogin();
+						uni.showModal({
+						  title: '授权失败',
+						  content: '获取授权信息失败，请重新授权登录',
+						  showCancel: false
+						});
+					}
+				});
+			},
+			//旧版登录接口
 			userlogin(e) {
 				let self = this;
 				let user = e.detail;
